8e808e5f157275d4ade10c4c1186915ff15e5b00
freemanmax <nenadstojkovikj@gmail.com>
2019-02-13 23:03:23 +0100

    Add show views for ads.

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,6 +1,20 @@
 class AdsController < ApplicationController
   def new
   end
+  def show
+      @ad = Ad.find(params[:id])
+  end
   def index
+  end
+  def create
+      @ad = Ad.new(ad_params)
+
+      @ad.save
+      redirect_to @ad
+  end
+
+  private
+  def ad_params
+      params.require(:ad).permit(:marka)
   end
 end
