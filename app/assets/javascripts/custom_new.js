facacf0922eff369d29c6e9ef395f81ca2603d24
freemanmax <nenadstojkovikj@gmail.com>
2019-03-09 04:47:44 +0100

    Fixing bug showing all model on page opening.

--- app/assets/javascripts/custom_new.js
+++ app/assets/javascripts/custom_new.js
@@ -3,19 +3,21 @@ $(document).ready(function(){
     var models;
     models = $('#ad_model').html();
     console.log(models);
+    $('#ad_model').empty();
+    $('#ad_model').html('<option value=""></option>');
     return $('#ad_marka').on("change", function(){
       var marka, escaped_marka, options;
       marka = $('#ad_marka :selected').text();
       escaped_marka = marka.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
       options = $(models).filter("optgroup[label='" + escaped_marka + "']").html();
       console.log(options);
-      if ((options) != null) {
+      if ((options) == null) {
         $('#ad_model').empty();
-        $('#ad_model').html(options);
+        $('#ad_model').html('<option value=""></option>');
       }
       else {
         $('#ad_model').empty();
-        $('#ad_model').html('<option value=""></option>');
+        $('#ad_model').html(options);
       }
     });
   });
